9ee09c3bd8427755c3555e422b77700c3ac12e34
Elham Alizadeh <92359726+elhamalizadeh@users.noreply.github.com>
2024-03-14 17:17:16 +0330

    feat:add services to home

---
+++ components/home/services.vue
@@ -0,0 +1,141 @@
+<template>
+    <div>
+        <div class="home-container089">
+          <span class="home-text037">Entry Visa Service</span>
+          <span class="home-text038">
+            <span>The smart services system provides a range of services</span>
+            <br />
+          </span>
+        </div>
+		   
+        <div class="home-container090">
+			
+			
+			<div class="d">
+				 <a href="#">
+			
+			
+          <div class="home-container091">
+			  
+			  
+			  <div class="hover01 column" >
+				  <figure>
+            <img
+              alt="image"
+              src="../../public/images/external/vis-01-1400w.jpg"
+              class="home-image21"
+            />
+				</figure>
+			 </div>
+			  	
+            <div class="home-container092" id="hide5">
+              <span class="home-text041">DUBAI VISA</span>
+              <span class="home-text042">Book Now</span>
+            </div>
+            <div class="home-container093" id="hide4">
+              <span class="home-text043">499$</span>
+              <span class="home-text044">
+				  <span>
+					<img
+                alt="image"
+                src="../../public/images/external/icone-1.png" class="icon3"
+               
+              />
+					</span>
+                <span>with in 3 to 5 Working days</span>
+                <br />
+                <span>Valid till 60 days for issuing</span>
+              </span>
+            </div>
+          </div>
+		</a>
+			</div>
+				
+			
+			<div class="d">
+				 <a href="#">
+			
+			
+          <div class="home-container091">
+			  
+			  
+			  <div class="hover01 column" >
+				  <figure>
+            <img
+              alt="image"
+              src="../../public/images/external/vis-03-1400w.jpg"
+              class="home-image22"
+            />
+				</figure>
+			 </div>
+			  	
+            <div class="home-container092" id="hide5">
+              <span class="home-text048">EGYPT VISA</span>
+              <span class="home-text042">Book Now</span>
+            </div>
+            <div class="home-container093" id="hide4">
+              <span class="home-text043">659$</span>
+              <span class="home-text044">
+				  <span>
+					<img
+                alt="image"
+                src="../../public/images/external/icone-1.png" class="icon3"
+               
+              />
+					</span>
+                <span>with in 3 to 5 Working days</span>
+                <br />
+                <span>Valid till 60 days for issuing</span>
+              </span>
+            </div>
+          </div>
+		</a>
+			</div>
+			
+			 
+				  
+
+			
+			<div class="d">
+				 <a href="#">
+			
+			
+          <div class="home-container091">
+			  
+			  
+			  <div class="hover01 column" >
+				  <figure>
+           <img
+              alt="image"
+              src="../../public/images/external/vis-02-1400w.jpg"
+              class="home-image23"
+            />
+				</figure>
+			 </div>
+			  	
+            <div class="home-container092" id="hide5">
+              <span class="home-text041">INDIA VISA</span>
+              <span class="home-text042">Book Now</span>
+            </div>
+            <div class="home-container093" id="hide4">
+              <span class="home-text043">499$</span>
+              <span class="home-text044">
+				  <span>
+					<img
+                alt="image"
+                src="../../public/images/external/icone-1.png" class="icon3"
+               
+              />
+					</span>
+                <span>with in 3 to 5 Working days</span>
+                <br />
+                <span>Valid till 60 days for issuing</span>
+              </span>
+            </div>
+          </div>
+		</a>
+			</div>			
+					
+        </div>
+    </div>
+</template>
