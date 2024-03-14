de33c0984ae72378f9ac5474868f31ca06466d28
Elham Alizadeh <92359726+elhamalizadeh@users.noreply.github.com>
2024-03-14 16:30:29 +0330

    feat: add menu component to flysearch page

---
+++ components/flySearch/menu.vue
@@ -0,0 +1,344 @@
+<template>
+    <div class="home-container001">
+           <header data-role="Header" class="home-header">
+             <div data-role="BurgerMenu" class="home-burger-menu">
+               <img
+                 alt="image"
+                 src="../../public/images/fly-search/ald-icn-2.-200h.png"
+                 class="home-image"
+               />
+             </div>
+             <div data-role="MobileMenu" class="home-mobile-menu">
+               <div class="home-filter">
+                 <div class="home-container002">
+                   <div class="home-container003">
+                     <span class="home-text">FILTER RESULT</span>
+                   </div>
+                   <div class="home-container004">
+                     <span class="home-text001">Payment price</span>
+                   </div>
+                   <div class="home-container005">
+                     <span class="home-text002">
+                       <span class="home-text003">
+                         $40&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                         &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; $9999
+                       </span>
+                       <br class="home-text004" />
+                       <span>●</span>
+                       <span class="home-text006">
+                         ----------------------------------
+                       </span>
+                       <span>●</span>
+                     </span>
+                   </div>
+                   <div class="home-container006">
+                     <button type="button" class="home-button button">
+                       Apply
+                     </button>
+                     <button type="button" class="home-button01 button">
+                       Reset
+                     </button>
+                   </div>
+                 </div>
+                 <div class="home-container007">
+                   <div class="home-container008">
+                     <span class="home-text008">Pauses</span>
+                   </div>
+                   <div class="home-container009">
+                     <svg viewBox="0 0 1024 1024" class="home-icon">
+                       <path
+                         d="M316 658l-60-60 256-256 256 256-60 60-196-196z"
+                       ></path>
+                     </svg>
+                   </div>
+                 </div>
+                 <div class="home-container010">
+                   <input
+                     type="checkbox"
+                     checked="true"
+                     class="home-checkbox"
+                   />
+                   <div class="home-container011">
+                     <span class="home-text009">non stop</span>
+                   </div>
+                 </div>
+                 <div class="home-container012">
+                   <input
+                     type="checkbox"
+                     checked="true"
+                     class="home-checkbox01"
+                   />
+                   <div class="home-container013">
+                     <span class="home-text010">One stop</span>
+                   </div>
+                 </div>
+                 <div class="home-container014">
+                   <input
+                     type="checkbox"
+                     checked="true"
+                     class="home-checkbox02"
+                   />
+                   <div class="home-container015">
+                     <span class="home-text011">Most of me stop</span>
+                   </div>
+                 </div>
+                 <div class="home-container016">
+                   <button type="button" class="home-button02 button">
+                     Apply
+                   </button>
+                   <button type="button" class="home-button03 button">
+                     Reset
+                   </button>
+                 </div>
+                 <div class="home-container017">
+                   <div class="home-container018">
+                     <span class="home-text012">Time to Leave</span>
+                   </div>
+                   <div class="home-container019">
+                     <svg viewBox="0 0 1024 1024" class="home-icon02">
+                       <path
+                         d="M316 658l-60-60 256-256 256 256-60 60-196-196z"
+                       ></path>
+                     </svg>
+                   </div>
+                 </div>
+                 <div class="home-container020">
+                   <input
+                     type="checkbox"
+                     checked="true"
+                     class="home-checkbox03"
+                   />
+                   <div class="home-container021">
+                     <span class="home-text013">Early morning</span>
+                   </div>
+                 </div>
+                 <div class="home-container022">
+                   <input
+                     type="checkbox"
+                     checked="true"
+                     class="home-checkbox04"
+                   />
+                   <div class="home-container023">
+                     <span class="home-text014">A.M.</span>
+                   </div>
+                 </div>
+                 <div class="home-container024">
+                   <input
+                     type="checkbox"
+                     checked="true"
+                     class="home-checkbox05"
+                   />
+                   <div class="home-container025">
+                     <span class="home-text015">Midday</span>
+                   </div>
+                 </div>
+                 <div class="home-container026">
+                   <button type="button" class="home-button04 button">
+                     Apply
+                   </button>
+                   <button type="button" class="home-button05 button">
+                     Reset
+                   </button>
+                 </div>
+                 <div class="home-container027">
+                   <div class="home-container028">
+                     <span class="home-text016">Airlines</span>
+                   </div>
+                   <div class="home-container029">
+                     <svg viewBox="0 0 1024 1024" class="home-icon04">
+                       <path
+                         d="M316 658l-60-60 256-256 256 256-60 60-196-196z"
+                       ></path>
+                     </svg>
+                   </div>
+                 </div>
+                 <div class="home-container030">
+                   <input
+                     type="checkbox"
+                     checked="true"
+                     class="home-checkbox06"
+                   />
+                   <div class="home-container031">
+                     <span class="home-text017">Iraqi Airways</span>
+                   </div>
+                 </div>
+                 <div class="home-container032">
+                   <input
+                     type="checkbox"
+                     checked="true"
+                     class="home-checkbox07"
+                   />
+                   <div class="home-container033">
+                     <span class="home-text018">Fly Baghdad</span>
+                   </div>
+                 </div>
+                 <div class="home-container034">
+                   <input
+                     type="checkbox"
+                     checked="true"
+                     class="home-checkbox08"
+                   />
+                   <div class="home-container035">
+                     <span class="home-text019">Fly Erbil</span>
+                   </div>
+                 </div>
+               </div>
+               <div class="home-container036">
+                 <div
+                   data-role="CloseMobileMenu"
+                   class="home-close-mobile-menu"
+                 >
+                   <svg viewBox="0 0 1024 1024" class="home-icon06">
+                     <path
+                       d="M810 274l-238 238 238 238-60 60-238-238-238 238-60-60 238-238-238-238 60-60 238 238 238-238z"
+                     ></path>
+                   </svg>
+                 </div>
+               </div>
+             </div>
+           </header>
+           <header data-role="Header" class="home-header1">
+             <img
+               alt="logo"
+               src="../../public/images/fly-search/logo-1500h.png"
+               class="home-image01"
+             />
+             <img
+               alt="image"
+               src="../../public/images/fly-search/logo-fotter-200h.png"
+               class="home-image02"
+             />
+             <div class="home-nav">
+               <nav
+                 class="navigation-links-nav navigation-links-root-class-name8"
+               >
+                 <span class="navigation-links-text"><span>Home</span></span>
+                 <span class="navigation-links-text1">
+                   <span>Flight</span>
+                 </span>
+                 <span class="navigation-links-text2"><span>Hotel</span></span>
+                 <span class="navigation-links-text3">
+                   <span>Package</span>
+                 </span>
+                 <span class="navigation-links-text4"><span>Visa</span></span>
+               </nav>
+             </div>
+             <div class="home-btn-group">
+               <button class="home-login button">Login</button>
+               <img
+                 alt="image"
+                 src="../../public/images/fly-search/iq-200h.png"
+                 class="home-image03"
+               />
+             </div>
+             <div data-role="BurgerMenu" class="home-burger-menu1">
+               <svg viewBox="0 0 1024 1024" class="home-icon08">
+                 <path
+                   d="M128 554.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 298.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 810.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667z"
+                 ></path>
+               </svg>
+             </div>
+             <div data-role="MobileMenu" class="home-mobile-menu1">
+               <nav class="home-nav1">
+                 <div class="home-container037">
+                   <img
+                     alt="image"
+                     src="../../public/images/fly-search/logo-mob-1500h-1500h.png"
+                     class="home-image04"
+                   />
+                   <div
+                     data-role="CloseMobileMenu"
+                     class="home-close-mobile-menu1"
+                   >
+                     <svg viewBox="0 0 1024 1024" class="home-icon10">
+                       <path
+                         d="M810 274l-238 238 238 238-60 60-238-238-238 238-60-60 238-238-238-238 60-60 238 238 238-238z"
+                       ></path>
+                     </svg>
+                   </div>
+                 </div>
+                 <div class="home-btn-group1">
+                   <button class="home-button06 button">Login / Join</button>
+                   <img
+                     alt="image"
+                     src="../../public/images/fly-search/iq-200h-200h-200h.png"
+                     class="home-image05"
+                   />
+                   <img
+                     alt="image"
+                     src="../../public/images/fly-search/earth-1681722_1280-1-200h-200h.png"
+                     class="home-image06"
+                   />
+                 </div>
+                 <nav
+                   class="navigation-links1-nav navigation-links1-root-class-name14"
+                 >
+                   <span class="navigation-links1-text">
+                     <span>Home</span>
+                   </span>
+                   <span class="navigation-links1-text1">
+                     <span>Flight</span>
+                   </span>
+                   <span class="navigation-links1-text2">
+                     <span>Hotel</span>
+                   </span>
+                   <span class="navigation-links1-text3">
+                     <span>Package</span>
+                   </span>
+                   <span class="navigation-links1-text4">
+                     <span>Visa</span>
+                   </span>
+                 </nav>
+               </nav>
+               <div class="home-icon-group">
+                 <img
+                   alt="image"
+                   src="../../public/images/fly-search/iata-logo-400h-400h.png"
+                   class="home-image07"
+                 />
+                 <img
+                   alt="image"
+                   src="../../public/images/fly-search/schemeaccepted-1-400h-400h.png"
+                   class="home-image08"
+                 />
+                 <img
+                   alt="image"
+                   src="../../public/images/fly-search/schemeaccepted-2-400h-400h.png"
+                   class="home-image09"
+                 />
+                  <svg viewBox="0 0 877.7142857142857 1024" class="home-icon12">
+                   <path
+                     d="M585.143 512c0-80.571-65.714-146.286-146.286-146.286s-146.286 65.714-146.286 146.286 65.714 146.286 146.286 146.286 146.286-65.714 146.286-146.286zM664 512c0 124.571-100.571 225.143-225.143 225.143s-225.143-100.571-225.143-225.143 100.571-225.143 225.143-225.143 225.143 100.571 225.143 225.143zM725.714 277.714c0 29.143-23.429 52.571-52.571 52.571s-52.571-23.429-52.571-52.571 23.429-52.571 52.571-52.571 52.571 23.429 52.571 52.571zM438.857 152c-64 0-201.143-5.143-258.857 17.714-20 8-34.857 17.714-50.286 33.143s-25.143 30.286-33.143 50.286c-22.857 57.714-17.714 194.857-17.714 258.857s-5.143 201.143 17.714 258.857c8 20 17.714 34.857 33.143 50.286s30.286 25.143 50.286 33.143c57.714 22.857 194.857 17.714 258.857 17.714s201.143 5.143 258.857-17.714c20-8 34.857-17.714 50.286-33.143s25.143-30.286 33.143-50.286c22.857-57.714 17.714-194.857 17.714-258.857s5.143-201.143-17.714-258.857c-8-20-17.714-34.857-33.143-50.286s-30.286-25.143-50.286-33.143c-57.714-22.857-194.857-17.714-258.857-17.714zM877.714 512c0 60.571 0.571 120.571-2.857 181.143-3.429 70.286-19.429 132.571-70.857 184s-113.714 67.429-184 70.857c-60.571 3.429-120.571 2.857-181.143 2.857s-120.571 0.571-181.143-2.857c-70.286-3.429-132.571-19.429-184-70.857s-67.429-113.714-70.857-184c-3.429-60.571-2.857-120.571-2.857-181.143s-0.571-120.571 2.857-181.143c3.429-70.286 19.429-132.571 70.857-184s113.714-67.429 184-70.857c60.571-3.429 120.571-2.857 181.143-2.857s120.571-0.571 181.143 2.857c70.286 3.429 132.571 19.429 184 70.857s67.429 113.714 70.857 184c3.429 60.571 2.857 120.571 2.857 181.143z"
+                   ></path>
+                 </svg> 
+                  <svg
+                   viewBox="0 0 877.7142857142857 1024"
+                   class="home-icon14"
+                 >
+                   <path
+                     d="M135.429 808h132v-396.571h-132v396.571zM276 289.143c-0.571-38.857-28.571-68.571-73.714-68.571s-74.857 29.714-74.857 68.571c0 37.714 28.571 68.571 73.143 68.571h0.571c46.286 0 74.857-30.857 74.857-68.571zM610.286 808h132v-227.429c0-121.714-65.143-178.286-152-178.286-70.857 0-102.286 39.429-119.429 66.857h1.143v-57.714h-132s1.714 37.143 0 396.571v0h132v-221.714c0-11.429 0.571-23.429 4-32 9.714-23.429 31.429-48 68-48 47.429 0 66.286 36 66.286 89.714v212zM877.714 237.714v548.571c0 90.857-73.714 164.571-164.571 164.571h-548.571c-90.857 0-164.571-73.714-164.571-164.571v-548.571c0-90.857 73.714-164.571 164.571-164.571h548.571c90.857 0 164.571 73.714 164.571 164.571z"
+                   ></path></svg>
+                 <svg
+                   viewBox="0 0 950.8571428571428 1024"
+                   class="home-icon16"
+                 >
+                   <path
+                     d="M925.714 233.143c-25.143 36.571-56.571 69.143-92.571 95.429 0.571 8 0.571 16 0.571 24 0 244-185.714 525.143-525.143 525.143-104.571 0-201.714-30.286-283.429-82.857 14.857 1.714 29.143 2.286 44.571 2.286 86.286 0 165.714-29.143 229.143-78.857-81.143-1.714-149.143-54.857-172.571-128 11.429 1.714 22.857 2.857 34.857 2.857 16.571 0 33.143-2.286 48.571-6.286-84.571-17.143-148-91.429-148-181.143v-2.286c24.571 13.714 53.143 22.286 83.429 23.429-49.714-33.143-82.286-89.714-82.286-153.714 0-34.286 9.143-65.714 25.143-93.143 90.857 112 227.429 185.143 380.571 193.143-2.857-13.714-4.571-28-4.571-42.286 0-101.714 82.286-184.571 184.571-184.571 53.143 0 101.143 22.286 134.857 58.286 41.714-8 81.714-23.429 117.143-44.571-13.714 42.857-42.857 78.857-81.143 101.714 37.143-4 73.143-14.286 106.286-28.571z"
+                   ></path></svg>
+                 <svg
+                   viewBox="0 0 602.2582857142856 1024"
+                   class="home-icon18"
+                 >
+                   <path
+                     d="M548 6.857v150.857h-89.714c-70.286 0-83.429 33.714-83.429 82.286v108h167.429l-22.286 169.143h-145.143v433.714h-174.857v-433.714h-145.714v-169.143h145.714v-124.571c0-144.571 88.571-223.429 217.714-223.429 61.714 0 114.857 4.571 130.286 6.857z"
+                   ></path></svg>
+                 <svg viewBox="0 0 1024 1024" class="home-icon20">
+                   <path
+                     d="M406.286 644.571l276.571-142.857-276.571-144.571v287.429zM512 152c215.429 0 358.286 10.286 358.286 10.286 20 2.286 64 2.286 102.857 43.429 0 0 31.429 30.857 40.571 101.714 10.857 82.857 10.286 165.714 10.286 165.714v77.714s0.571 82.857-10.286 165.714c-9.143 70.286-40.571 101.714-40.571 101.714-38.857 40.571-82.857 40.571-102.857 42.857 0 0-142.857 10.857-358.286 10.857v0c-266.286-2.286-348-10.286-348-10.286-22.857-4-74.286-2.857-113.143-43.429 0 0-31.429-31.429-40.571-101.714-10.857-82.857-10.286-165.714-10.286-165.714v-77.714s-0.571-82.857 10.286-165.714c9.143-70.857 40.571-101.714 40.571-101.714 38.857-41.143 82.857-41.143 102.857-43.429 0 0 142.857-10.286 358.286-10.286v0z"
+                   ></path>
+                 </svg> 
+               </div>
+             </div>
+           </header>
+         </div>
+</template>
