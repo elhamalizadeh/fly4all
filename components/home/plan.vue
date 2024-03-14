d878fc7a90c2e8bea9394b5075e8b2b54255f2ea
Elham Alizadeh <92359726+elhamalizadeh@users.noreply.github.com>
2024-03-14 17:14:07 +0330

    feat: add plan component to home

---
+++ components/home/plan.vue
@@ -0,0 +1,118 @@
+<template>
+    <div>
+        <div class="home-container059">
+          <span class="home-text025">Plan for Unforgettable Trips</span>
+          <span class="home-text026">
+            <span>Decide on your destination and consider what time</span>
+            <br />
+          </span>
+        </div>
+        <div class="home-container060">
+          <div class="home-container061">
+            <div class="home-container062">
+              <div class="hover01 column">
+              <div>
+              <figure> <a href="#"><img
+                alt="image"
+                src="../../public/images/external/car-services-by-d-and-t-1400w.jpg"
+                class="home-image17"
+              /></a>
+				  </figure>
+			  </div>
+			  </div>
+              <div class="home-container063">
+                <div class="home-container064">
+                  <div class="home-container065">
+                    <div class="home-container066">
+                      <span class="home-text029">With Offers FLY4ALL</span>
+                    </div>
+                    <div class="home-container067">
+                      <span class="home-text030">
+                        Travel at the Best Prices
+                      </span>
+                    </div>
+                  </div>
+                  <div class="home-container068">
+                    <button type="button" class="home-button10 button">
+                      →
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="home-container069"> <a href="#"><img
+                alt="image"
+                src="../../public/images/external/mobile0012-1400w.jpg"
+                class="home-image18"
+              /></a>
+              <div class="home-container070">
+                <div class="home-container071">
+                  <div class="home-container072">
+                    <div class="home-container073">
+                      <span class="home-text031">With Offers FLY4ALL</span>
+                    </div>
+                    <div class="home-container074">
+                      <span class="home-text032">
+                        Travel at the Best Prices
+                      </span>
+                    </div>
+                  </div>
+                  <div class="home-container075">
+                    <button type="button" class="home-button11 button">
+                      →
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="home-container076"> <a href="#"><img
+              alt="image"
+              src="../../public/images/external/iberoab1wwerahsnnpvq__34280-1400w.jpg"
+              class="home-image19"
+            /></a>
+				
+            <div class="home-container077">
+              <div class="home-container078">
+                <div class="home-container079">
+                  <div class="home-container080">
+                    <span class="home-text033">With Offers FLY4ALL</span>
+                  </div>
+                  <div class="home-container081">
+                    <span class="home-text034">Travel at the Best Prices</span>
+                  </div>
+                </div>
+                <div class="home-container082">
+                  <button type="button" class="home-button12 button">→</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="home-container083">
+            <div class="hover01 column">
+              <div>
+              <figure> <a href="#"><img
+              alt="image"
+              src="../../public/images/external/gift-certificate-for-honeymoon-travel-1400w.jpg"
+              class="home-image20"
+            /></a>
+			    </figure>
+			  </div>
+		    </div>
+            <div class="home-container084">
+              <div class="home-container085">
+                <div class="home-container086">
+                  <span class="home-text035">With Offers FLY4ALL</span>
+                </div>
+                <div class="home-container087">
+                  <span class="home-text036">Travel at the Best Prices</span>
+                </div>
+              </div>
+              <div class="home-container088">
+                <button type="button" class="home-button13 button">→</button>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -202,6 +202,8 @@
     </div>
     <!---------->
     <HomeRecomended />
+
+    <HomePlan />
   </div>
 </template>
   
