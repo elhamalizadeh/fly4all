2068b516ce5c0fc57b4b251b2704fc81f382b576
Elham Alizadeh <92359726+elhamalizadeh@users.noreply.github.com>
2024-03-14 16:32:13 +0330

    feat: add form component to fly-search page

---
+++ components/flySearch/form.vue
@@ -0,0 +1,129 @@
+<template>
+    <div class="home-container041">
+                  <div class="home-container042">
+                    <div class="home-container043">
+                      <input
+                        type="radio"
+                        name="radio"
+                        class="home-radiobutton"
+                      />
+                      <span class="home-text020">All flights</span>
+                    </div>
+                    <div class="home-container044">
+                      <input
+                        type="radio"
+                        name="radio"
+                        class="home-radiobutton1"
+                      />
+                      <span class="home-text021">Charter only</span>
+                    </div>
+                    <div class="home-container045"></div>
+                    <div class="home-container046"></div>
+                    <div class="home-container047"></div>
+                    <div class="home-container048"></div>
+                  </div>
+                  <select class="home-select">
+                    <option value="Option 1">Option 1</option>
+                    <option value="Option 2">Option 2</option>
+                    <option value="Option 3">Option 3</option>
+                  </select>
+                  <select class="home-select1">
+                    <option value="Option 1">Option 1</option>
+                    <option value="Option 1">Option 1</option>
+                    <option value="Option 2">Option 2</option>
+                    <option value="Option 2">Option 2</option>
+                    <option value="Option 3">Option 3</option>
+                    <option value="Option 3">Option 3</option>
+                  </select>
+                  <select class="home-select2">
+                    <option value="Option 1">Option 1</option>
+                    <option value="Option 1">Option 1</option>
+                    <option value="Option 1">Option 1</option>
+                    <option value="Option 2">Option 2</option>
+                    <option value="Option 2">Option 2</option>
+                    <option value="Option 2">Option 2</option>
+                    <option value="Option 3">Option 3</option>
+                    <option value="Option 3">Option 3</option>
+                    <option value="Option 3">Option 3</option>
+                  </select>
+                  <div class="home-container049">
+                    <div class="home-container050">
+                      <input
+                        type="text"
+                        placeholder="From"
+                        class="home-textinput input"
+                      />
+                    </div>
+                    <!-- <div class="home-container051">
+                      <svg viewBox="0 0 1024 1024" class="home-icon22">
+                        <path
+                          d="M942 412q6 26-7 48t-39 30q-248 66-412 110l-226 60-68 20-112-192 62-16 84 64 212-56-176-306 82-22 294 274 228-60q26-8 49 6t29 40zM106 810h812v86h-812v-86z"
+                        ></path>
+                      </svg>
+                    </div> -->
+                    <!-- <div class="home-container052">
+                      <svg viewBox="0 0 1024 1024" class="home-icon24">
+                        <path
+                          d="M320 384h128v128h-128zM512 384h128v128h-128zM704 384h128v128h-128zM128 768h128v128h-128zM320 768h128v128h-128zM512 768h128v128h-128zM320 576h128v128h-128zM512 576h128v128h-128zM704 576h128v128h-128zM128 576h128v128h-128zM832 0v64h-128v-64h-448v64h-128v-64h-128v1024h960v-1024h-128zM896 960h-832v-704h832v704z"
+                        ></path>
+                      </svg>
+                    </div> -->
+                    <!-- <div class="home-container053">
+                      <svg viewBox="0 0 1024 1024" class="home-icon26">
+                        <path
+                          d="M170.667 384h149.333c31.403 0 59.435 16.683 78.976 42.709 13.867-25.771 31.104-49.621 50.816-70.912-34.261-35.115-79.616-57.131-129.792-57.131h-149.333c-23.595 0-42.667 19.115-42.667 42.667s19.072 42.667 42.667 42.667z"
+                        ></path>
+                        <path
+                          d="M498.56 516.736c23.509-70.699 96.256-132.736 155.691-132.736h78.421l-55.168 55.168c-16.683 16.683-16.683 43.648 0 60.331 8.32 8.32 19.243 12.501 30.165 12.501s21.845-4.181 30.165-12.501l158.165-158.165-158.165-158.165c-16.683-16.683-43.648-16.683-60.331 0s-16.683 43.648 0 60.331l55.168 55.168h-78.421c-97.024 0-201.003 83.925-236.672 191.104l-20.139 60.203c-27.349 82.176-88.405 132.693-120.107 132.693h-106.667c-23.595 0-42.667 19.115-42.667 42.667s19.072 42.667 42.667 42.667h106.667c78.379 0 164.821-82.133 201.088-191.104l20.139-60.16z"
+                        ></path>
+                        <path
+                          d="M677.504 567.168c-16.683 16.683-16.683 43.648 0 60.331l55.168 55.168h-99.755c-54.101 0-99.413-38.016-114.816-89.941-10.923 32-26.752 63.957-46.507 93.227 37.803 49.579 95.701 82.048 161.323 82.048h99.755l-55.168 55.168c-16.683 16.683-16.683 43.648 0 60.331 8.32 8.32 19.243 12.501 30.165 12.501s21.845-4.181 30.165-12.501l158.165-158.165-158.165-158.165c-16.683-16.683-43.648-16.683-60.331 0z"
+                        ></path>
+                      </svg>
+                    </div> -->
+                    <div class="home-container054">
+                      <input
+                        type="text"
+                        placeholder="To"
+                        class="home-textinput1 input"
+                      />
+                    </div>
+                    <!-- <div class="home-container055">
+                      <svg viewBox="0 0 1024 1024" class="home-icon30">
+                        <path
+                          d="M598 616q-164-46-412-110l-68-20v-220l62 16 40 100 212 56v-352l82 22 118 384 226 60q26 8 39 31t7 49q-8 26-30 38t-48 6zM106 810h812v86h-812v-86z"
+                        ></path>
+                      </svg>
+                    </div> -->
+                    <div class="home-container056">
+                      <input
+                        type="text"
+                        placeholder="Date"
+                        class="home-textinput2 input"
+                      />
+                    </div>
+                    <div class="home-container057">
+                      <input
+                        type="text"
+                        placeholder="Travelers "
+                        class="home-textinput3 input"
+                      />
+                    </div>
+                    <!-- <div class="home-container058">
+                      <svg
+                        viewBox="0 0 1097.142857142857 1024"
+                        class="home-icon32"
+                      >
+                        <path
+                          d="M338.857 512c-59.429 1.714-113.143 27.429-151.429 73.143h-76.571c-57.143 0-110.857-27.429-110.857-90.857 0-46.286-1.714-201.714 70.857-201.714 12 0 71.429 48.571 148.571 48.571 26.286 0 51.429-4.571 76-13.143-1.714 12.571-2.857 25.143-2.857 37.714 0 52 16.571 103.429 46.286 146.286zM950.857 876c0 92.571-61.143 148-152.571 148h-499.429c-91.429 0-152.571-55.429-152.571-148 0-129.143 30.286-327.429 197.714-327.429 19.429 0 90.286 79.429 204.571 79.429s185.143-79.429 204.571-79.429c167.429 0 197.714 198.286 197.714 327.429zM365.714 146.286c0 80.571-65.714 146.286-146.286 146.286s-146.286-65.714-146.286-146.286 65.714-146.286 146.286-146.286 146.286 65.714 146.286 146.286zM768 365.714c0 121.143-98.286 219.429-219.429 219.429s-219.429-98.286-219.429-219.429 98.286-219.429 219.429-219.429 219.429 98.286 219.429 219.429zM1097.143 494.286c0 63.429-53.714 90.857-110.857 90.857h-76.571c-38.286-45.714-92-71.429-151.429-73.143 29.714-42.857 46.286-94.286 46.286-146.286 0-12.571-1.143-25.143-2.857-37.714 24.571 8.571 49.714 13.143 76 13.143 77.143 0 136.571-48.571 148.571-48.571 72.571 0 70.857 155.429 70.857 201.714zM1024 146.286c0 80.571-65.714 146.286-146.286 146.286s-146.286-65.714-146.286-146.286 65.714-146.286 146.286-146.286 146.286 65.714 146.286 146.286z"
+                        ></path>
+                      </svg>
+                    </div> -->
+                  </div>
+                  <div class="home-container059">
+                    <button type="button" class="home-button07 button">
+                      Destination Now →
+                    </button>
+                  </div>
+                </div>
+</template>
