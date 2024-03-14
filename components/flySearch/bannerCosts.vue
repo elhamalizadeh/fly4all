7ae821ec00cbf8272736222b1f79eb32425ac483
Elham Alizadeh <92359726+elhamalizadeh@users.noreply.github.com>
2024-03-14 16:33:41 +0330

    feat: add bannerCost component to fly-search page

---
+++ components/flySearch/bannerCosts.vue
@@ -0,0 +1,88 @@
+<template>
+    <div>
+        <div class="home-container096">
+                <div class="home-container097">
+                  <span class="home-text042">Su, Jan 5</span>
+                  <span class="home-text043">253$</span>
+                </div>
+                <div class="home-container098">
+                  <span class="home-text044">Sa, Jan 4</span>
+                  <span class="home-text045">990$</span>
+                </div>
+                <div class="home-container099">
+                  <span class="home-text046">Sa, Jan 4</span>
+                  <span class="home-text047">970$</span>
+                </div>
+                <div class="home-container100">
+                  <span class="home-text048">Fr, Jan 3</span>
+                  <span class="home-text049">1998$</span>
+                </div>
+                <div class="home-container101">
+                  <span class="home-text050">Fr, Jan 3</span>
+                  <span class="home-text051">500$</span>
+                </div>
+                <div class="home-container102">
+                  <span class="home-text052">Th, Jan 2</span>
+                  <span class="home-text053">700$</span>
+                </div>
+                <div class="home-container103">
+                  <span class="home-text054">We, Jan 1</span>
+                  <span class="home-text055">1550$</span>
+                </div>
+              </div>
+              <div class="home-container104"></div>
+              <div class="home-container105">
+                <div class="home-container106">
+                  <img
+                    alt="image"
+                    src="../../public/images/fly-search/iraqi_airways_logo1-500w.png"
+                    loading="lazy"
+                    class="home-image10"
+                  />
+                  <span class="home-text056">One stop</span>
+                  <span class="home-text057">1998$</span>
+                </div>
+                <div class="home-container107">
+                  <img
+                    alt="image"
+                    src="../../public/images/fly-search/fly_baghdad_logo-200h.jpg"
+                    loading="lazy"
+                    class="home-image11"
+                  />
+                  <span class="home-text058">One stop</span>
+                  <span class="home-text059">1998$</span>
+                </div>
+                <div class="home-container108">
+                  <img
+                    alt="image"
+                    src="../../public/images/fly-search/iraqi_airways_logo1-500w.png"
+                    loading="lazy"
+                    class="home-image12"
+                  />
+                  <span class="home-text060">One stop</span>
+                  <span class="home-text061">1998$</span>
+                </div>
+                <div class="home-container109">
+                  <img
+                    alt="image"
+                    src="../../public/images/fly-search/fly_baghdad_logo-200h.jpg"
+                    loading="lazy"
+                    class="home-image13"
+                  />
+                  <span class="home-text062">One stop</span>
+                  <span class="home-text063">1998$</span>
+                </div>
+                <div class="home-container110">
+                  <img
+                    alt="image"
+                    src="../../public/images/fly-search/iraqi_airways_logo1-500w.png"
+                    loading="lazy"
+                    class="home-image14"
+                  />
+                  <span class="home-text064">One stop</span>
+                  <span class="home-text065">1998$</span>
+                </div>
+              </div>
+              <div class="home-container111"></div>
+    </div>
+</template>
