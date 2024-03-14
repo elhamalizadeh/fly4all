0bf2724d87d050f70171b5735ae1681aeca08a54
Elham Alizadeh <92359726+elhamalizadeh@users.noreply.github.com>
2024-03-14 16:35:03 +0330

    feat:add sidebar component to fly-search page

---
+++ components/flySearch/sidebar.vue
@@ -0,0 +1,140 @@
+<template>
+    <div class="home-filter1">
+             <div class="home-container061">
+               <div class="home-container062">
+                 <span class="home-text022">FILTER RESULT</span>
+               </div>
+               <div class="home-container063">
+                 <span class="home-text023">Payment price</span>
+               </div>
+               <div class="home-container064">
+                 <span class="home-text024">
+                   <span class="home-text025">
+                     $40&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                     &nbsp; &nbsp; &nbsp; &nbsp; $9999
+                   </span>
+                   <br class="home-text026" />
+                   <span>●</span>
+                   <span class="home-text028">
+                     ----------------------------------
+                   </span>
+                   <span>●</span>
+                 </span>
+               </div>
+               <div class="home-container065">
+                 <button type="button" class="home-button08 button">
+                   Apply
+                 </button>
+                 <button type="button" class="home-button09 button">
+                   Reset
+                 </button>
+               </div>
+             </div>
+             <div class="home-container066">
+               <div class="home-container067">
+                 <span class="home-text030">Pauses</span>
+               </div>
+               <!-- <div class="home-container068">
+                 <svg viewBox="0 0 1024 1024" class="home-icon34">
+                   <path
+                     d="M316 658l-60-60 256-256 256 256-60 60-196-196z"
+                   ></path>
+                 </svg>
+               </div> -->
+             </div>
+             <div class="home-container069">
+               <input type="checkbox" checked="true" class="home-checkbox09" />
+               <div class="home-container070">
+                 <span class="home-text031">non stop</span>
+               </div>
+             </div>
+             <div class="home-container071">
+               <input type="checkbox" checked="true" class="home-checkbox10" />
+               <div class="home-container072">
+                 <span class="home-text032">One stop</span>
+               </div>
+             </div>
+             <div class="home-container073">
+               <input type="checkbox" checked="true" class="home-checkbox11" />
+               <div class="home-container074">
+                 <span class="home-text033">Most of me stop</span>
+               </div>
+             </div>
+             <div class="home-container075">
+               <button type="button" class="home-button10 button">
+                 Apply
+               </button>
+               <button type="button" class="home-button11 button">
+                 Reset
+               </button>
+             </div>
+             <div class="home-container076">
+               <div class="home-container077">
+                 <span class="home-text034">Time to Leave</span>
+               </div>
+               <!-- <div class="home-container078">
+                 <svg viewBox="0 0 1024 1024" class="home-icon36">
+                   <path
+                     d="M316 658l-60-60 256-256 256 256-60 60-196-196z"
+                   ></path>
+                 </svg>
+               </div> -->
+             </div>
+             <div class="home-container079">
+               <input type="checkbox" checked="true" class="home-checkbox12" />
+               <div class="home-container080">
+                 <span class="home-text035">Early morning</span>
+               </div>
+             </div>
+             <div class="home-container081">
+               <input type="checkbox" checked="true" class="home-checkbox13" />
+               <div class="home-container082">
+                 <span class="home-text036">A.M.</span>
+               </div>
+             </div>
+             <div class="home-container083">
+               <input type="checkbox" checked="true" class="home-checkbox14" />
+               <div class="home-container084">
+                 <span class="home-text037">Midday</span>
+               </div>
+             </div>
+             <div class="home-container085">
+               <button type="button" class="home-button12 button">
+                 Apply
+               </button>
+               <button type="button" class="home-button13 button">
+                 Reset
+               </button>
+             </div>
+             <div class="home-container086">
+               <div class="home-container087">
+                 <span class="home-text038">Airlines</span>
+               </div>
+               <!-- <div class="home-container088">
+                 <svg viewBox="0 0 1024 1024" class="home-icon38">
+                   <path
+                     d="M316 658l-60-60 256-256 256 256-60 60-196-196z"
+                   ></path>
+                 </svg>
+               </div> -->
+             </div>
+             <div class="home-container089">
+               <input type="checkbox" checked="true" class="home-checkbox15" />
+               <div class="home-container090">
+                 <span class="home-text039">Iraqi Airways</span>
+               </div>
+             </div>
+             <div class="home-container091">
+               <input type="checkbox" checked="true" class="home-checkbox16" />
+               <div class="home-container092">
+                 <span class="home-text040">Fly Baghdad</span>
+               </div>
+             </div>
+             <div class="home-container093">
+               <input type="checkbox" checked="true" class="home-checkbox17" />
+               <div class="home-container094">
+                 <span class="home-text041">Fly Erbil</span>
+               </div>
+             </div>
+           </div>
+</template>
