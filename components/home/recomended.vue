832071d467f61bd0b0fb3aa850dda388d7904332
Elham Alizadeh <92359726+elhamalizadeh@users.noreply.github.com>
2024-03-14 17:09:57 +0330

    feat:add recomended cmp to home

---
+++ components/home/recomended.vue
@@ -0,0 +1,181 @@
+<template>
+  <div>
+    <div class="home-container030">
+      <span class="home-text009">More Recommended Destination</span>
+      <span class="home-text010">
+        <span>Book Domestic and International Holiday Packages</span>
+        <br />
+      </span>
+    </div>
+
+    <div class="home-container031">
+      <div class="d">
+        <a href="#">
+          <div class="home-container050">
+            <div class="hover01 column">
+              <figure>
+                <img
+                  alt="image"
+                  src="../../public/images/external/new-baner-03-1400w.jpg"
+                  class="home-image11"
+                />
+              </figure>
+            </div>
+
+            <section class="home-container033" id="hide7">
+              <div class="home-container034">
+                <div class="home-container035">
+                  <div class="home-container036">
+                    <span class="home-text013">BAGHDAD</span>
+                  </div>
+                  <div class="home-container037">
+                    <div class="home-container038">
+                      <img
+                        alt="image"
+                        src="../../public/images/external/icon-01-200h.png"
+                        class="home-image12"
+                      />
+                    </div>
+                  </div>
+                  <div class="home-container039">
+                    <span class="home-text014">MASHHAD</span>
+                  </div>
+                </div>
+                <div class="home-container040">
+                  <button type="button" class="home-button04 button">
+                    <span class="home-text015">Facial services&nbsp;</span>
+                    <svg viewBox="0 0 1024 1024" class="home-icon26">
+                      <path
+                        d="M366 708l196-196-196-196 60-60 256 256-256 256z"
+                      ></path>
+                    </svg>
+                  </button>
+                  <button type="button" class="home-button05 button">
+                    <span class="home-text016">Search flights</span>
+                    <svg viewBox="0 0 1024 1024" class="home-icon28">
+                      <path
+                        d="M406 598q80 0 136-56t56-136-56-136-136-56-136 56-56 136 56 136 136 56zM662 598l212 212-64 64-212-212v-34l-12-12q-76 66-180 66-116 0-197-80t-81-196 81-197 197-81 196 81 80 197q0 42-20 95t-46 85l12 12h34z"
+                      ></path>
+                    </svg>
+                  </button>
+                </div>
+              </div>
+            </section>
+          </div>
+        </a>
+      </div>
+
+      <div class="d">
+        <a href="#">
+          <div class="home-container050">
+            <div class="hover01 column">
+              <figure>
+                <img
+                  alt="image"
+                  src="../../public/images/external/new-baner-01-1400w.jpg"
+                  class="home-image11"
+                />
+              </figure>
+            </div>
+
+            <section class="home-container042" id="hide7">
+              <div class="home-container043">
+                <div class="home-container044">
+                  <div class="home-container045">
+                    <span class="home-text017">BAGHDAD</span>
+                  </div>
+                  <div class="home-container046">
+                    <div class="home-container047">
+                      <img
+                        alt="image"
+                        src="../../public/images/external/icon-01-200h.png"
+                        class="home-image14"
+                      />
+                    </div>
+                  </div>
+                  <div class="home-container048">
+                    <span class="home-text018">BEIRUT</span>
+                  </div>
+                </div>
+                <div class="home-container049">
+                  <button type="button" class="home-button06 button">
+                    <span class="home-text019">Facial services&nbsp;</span>
+                    <svg viewBox="0 0 1024 1024" class="home-icon30">
+                      <path
+                        d="M366 708l196-196-196-196 60-60 256 256-256 256z"
+                      ></path>
+                    </svg>
+                  </button>
+                  <button type="button" class="home-button07 button">
+                    <span class="home-text020">Search flights</span>
+                    <svg viewBox="0 0 1024 1024" class="home-icon32">
+                      <path
+                        d="M406 598q80 0 136-56t56-136-56-136-136-56-136 56-56 136 56 136 136 56zM662 598l212 212-64 64-212-212v-34l-12-12q-76 66-180 66-116 0-197-80t-81-196 81-197 197-81 196 81 80 197q0 42-20 95t-46 85l12 12h34z"
+                      ></path>
+                    </svg>
+                  </button>
+                </div>
+              </div>
+            </section>
+          </div>
+        </a>
+      </div>
+
+      <div class="d">
+        <a href="#">
+          <div class="home-container050">
+            <div class="hover01 column">
+              <figure>
+                <img
+                  alt="image"
+                  src="../../public/images/external/new-baner-02-1400w.jpg"
+                  class="home-image11"
+                />
+              </figure>
+            </div>
+
+            <section class="home-container051" id="hide7">
+              <div class="home-container052">
+                <div class="home-container053">
+                  <div class="home-container054">
+                    <span class="home-text021">BAGHDAD</span>
+                  </div>
+                  <div class="home-container055">
+                    <div class="home-container056">
+                      <img
+                        alt="image"
+                        src="../../public/images/external/icon-01-200h.png"
+                        class="home-image16"
+                      />
+                    </div>
+                  </div>
+                  <div class="home-container057">
+                    <span class="home-text022">DUBAI</span>
+                  </div>
+                </div>
+                <div class="home-container058">
+                  <button type="button" class="home-button08 button">
+                    <span class="home-text023">Facial services&nbsp;</span>
+                    <svg viewBox="0 0 1024 1024" class="home-icon34">
+                      <path
+                        d="M366 708l196-196-196-196 60-60 256 256-256 256z"
+                      ></path>
+                    </svg>
+                  </button>
+                  <button type="button" class="home-button09 button">
+                    <span class="home-text024">Search flights</span>
+                    <svg viewBox="0 0 1024 1024" class="home-icon36">
+                      <path
+                        d="M406 598q80 0 136-56t56-136-56-136-136-56-136 56-56 136 56 136 136 56zM662 598l212 212-64 64-212-212v-34l-12-12q-76 66-180 66-116 0-197-80t-81-196 81-197 197-81 196 81 80 197q0 42-20 95t-46 85l12 12h34z"
+                      ></path>
+                    </svg>
+                  </button>
+                </div>
+              </div>
+            </section>
+          </div>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,215 +1,225 @@
 <template>
-    <div>
-      <section class="home-container003">
-        <div class="home-image10">
-          <div class="slider-one">
-            <div class="slider-one-image">
-              <div class="slider-text" id="all-test-slide">
-                <h1 id="text-slide">
-                  Istanbul: a jewel of culture and a bridge between continents
-                </h1>
-                <p id="text-small-slide">
-                  Explore the beauty of East and West at the heart of Istanbul's
-                  history, a city that embodies luxury and diversity.
-                </p>
-                <br />
-  
-                <button type="button" class="home-button03 button">
-                  Destination Now →
-                </button>
-              </div>
-            </div>
-          </div>
-          <div class="slider-two">
-            <div class="slider-two-image">
-              <div class="slider-text" id="all-test-slide">
-                <h1 id="text-slide">
-                  Dubai: More wonderful experiences await you in UAE!
-                </h1>
-                <p id="text-small-slide">
-                  Enjoy a unique experience in the Emirates, where history meets
-                  modernity and nature meets luxury!
-                </p>
-                <br />
-  
-                <button type="button" class="home-button03 button">
-                  Destination Now →
-                </button>
-              </div>
-            </div>
-          </div>
-          <div class="slider-three">
-            <div class="slider-three-image">
-              <div class="slider-text" id="all-test-slide">
-                <h1 id="text-slide">
-                  Istanbul: a jewel of culture and a bridge between continents
-                </h1>
-                <p id="text-small-slide">
-                  Explore the beauty of East and West at the heart of Istanbul's
-                  history, a city that embodies luxury and diversity.
-                </p>
-                <br />
-  
-                <button type="button" class="home-button03 button">
-                  Destination Now →
-                </button>
-              </div>
-            </div>
-          </div>
-          <div class="slider-four">
-            <div class="slider-four-image">
-              <div class="slider-text" id="all-test-slide">
-                <h1 id="text-slide">
-                  France: Experience the pleasure of seeing the natural landscapes
-                  of Europe
-                </h1>
-                <p id="text-small-slide">
-                  France is a country with a diverse range of natural beauty, from
-                  the rugged coastlines of Brittany to the majestic ...
-                </p>
-                <br />
-  
-                <button type="button" class="home-button03 button">
-                  Destination Now →
-                </button>
-              </div>
+  <div>
+    <section class="home-container003">
+      <div class="home-image10">
+        <div class="slider-one">
+          <div class="slider-one-image">
+            <div class="slider-text" id="all-test-slide">
+              <h1 id="text-slide">
+                Istanbul: a jewel of culture and a bridge between continents
+              </h1>
+              <p id="text-small-slide">
+                Explore the beauty of East and West at the heart of Istanbul's
+                history, a city that embodies luxury and diversity.
+              </p>
+              <br />
+
+              <button type="button" class="home-button03 button">
+                Destination Now →
+              </button>
             </div>
           </div>
         </div>
-  
-        <div class="home-container004">
-          <div class="home-container005">
-            <div class="home-container006">
-              <span class="home-text">FLIGHT</span>
+        <div class="slider-two">
+          <div class="slider-two-image">
+            <div class="slider-text" id="all-test-slide">
+              <h1 id="text-slide">
+                Dubai: More wonderful experiences await you in UAE!
+              </h1>
+              <p id="text-small-slide">
+                Enjoy a unique experience in the Emirates, where history meets
+                modernity and nature meets luxury!
+              </p>
+              <br />
+
+              <button type="button" class="home-button03 button">
+                Destination Now →
+              </button>
             </div>
-            <NuxtLink to="./fly4all-hotel" class="home-link1">
-              <div class="home-container007">
-                <span class="home-text001">HOTEL</span>
-              </div>
-            </NuxtLink>
-            <NuxtLink to="./fly4all-package" class="home-link1">
-              <div class="home-container008">
-                <span class="home-text002">Package</span>
-              </div>
-            </NuxtLink>
-            <NuxtLink to="./fly4all-visa" class="home-link1">
-              <div class="home-container009">
-                <span class="home-text003">VISA</span>
-              </div></NuxtLink
-            >
-            <div class="home-container010">
-              <div class="home-container011">
-                <input
-                  type="radio"
-                  name="radio"
-                  class="home-radiobutton"
-                  checked
-                />
-                <span class="home-text004">One-Way</span>
-              </div>
-              <div class="home-container012">
-                <input type="radio" name="radio" class="home-radiobutton1" />
-                <span class="home-text005">Round-Trip</span>
-              </div>
-  
-              <div class="home-container014"></div>
-              <div class="home-container015"></div>
-              <div class="home-container016"></div>
+          </div>
+        </div>
+        <div class="slider-three">
+          <div class="slider-three-image">
+            <div class="slider-text" id="all-test-slide">
+              <h1 id="text-slide">
+                Istanbul: a jewel of culture and a bridge between continents
+              </h1>
+              <p id="text-small-slide">
+                Explore the beauty of East and West at the heart of Istanbul's
+                history, a city that embodies luxury and diversity.
+              </p>
+              <br />
+
+              <button type="button" class="home-button03 button">
+                Destination Now →
+              </button>
             </div>
-            <div class="home-select">
+          </div>
+        </div>
+        <div class="slider-four">
+          <div class="slider-four-image">
+            <div class="slider-text" id="all-test-slide">
+              <h1 id="text-slide">
+                France: Experience the pleasure of seeing the natural landscapes
+                of Europe
+              </h1>
+              <p id="text-small-slide">
+                France is a country with a diverse range of natural beauty, from
+                the rugged coastlines of Brittany to the majestic ...
+              </p>
+              <br />
+
+              <button type="button" class="home-button03 button">
+                Destination Now →
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="home-container004">
+        <div class="home-container005">
+          <div class="home-container006">
+            <span class="home-text">FLIGHT</span>
+          </div>
+          <NuxtLink to="./fly4all-hotel" class="home-link1">
+            <div class="home-container007">
+              <span class="home-text001">HOTEL</span>
+            </div>
+          </NuxtLink>
+          <NuxtLink to="./fly4all-package" class="home-link1">
+            <div class="home-container008">
+              <span class="home-text002">Package</span>
+            </div>
+          </NuxtLink>
+          <NuxtLink to="./fly4all-visa" class="home-link1">
+            <div class="home-container009">
+              <span class="home-text003">VISA</span>
+            </div></NuxtLink
+          >
+          <div class="home-container010">
+            <div class="home-container011">
+              <input
+                type="radio"
+                name="radio"
+                class="home-radiobutton"
+                checked
+              />
+              <span class="home-text004">One-Way</span>
+            </div>
+            <div class="home-container012">
               <input type="radio" name="radio" class="home-radiobutton1" />
-              <span class="home-text005">Multi-destination</span>
+              <span class="home-text005">Round-Trip</span>
             </div>
-            <select class="home-select1">
-              <option value="Option 1">Economy</option>
-              <option value="Option 1">Business</option>
-              <option value="Option 2">First</option>
-            </select>
-  
-            <div class="home-container017">
-              <div class="home-container018">
-                <select class="home-textinput input" v-model="selectedSource">
-                  <option disabled value="">Search by city or airport</option>
-                  <option value="dubai">
-                    All airports in dubai: United Arab Emirates
-                  </option>
-                  <option value="istanbul">
-                    All airports in Istanbul: Turkey
-                  </option>
-                  <option value="tehran">All airports in Iran: Tehran</option>
-                </select>
-              </div>
-              <div class="home-container022">
-                <select
-                  class="home-textinput1 input"
-                  v-model="selectedDestination"
-                >
-                  <option disabled value="">Search by city or airport</option>
-                  <option value="dubai">
-                    All airports in dubai: United Arab Emirates
-                  </option>
-                  <option value="istanbul">
-                    All airports in Istanbul: Turkey
-                  </option>
-                  <option value="tehran">All airports in Iran: Tehran</option>
-                </select>
-              </div>
-              <div class="home-container024">
-                <input
-                  type="date"
-                  class="home-textinput2 input"
-                  v-model="selectedDate"
-                />
-              </div>
-              <div class="home-container025">
-                <input
-                  type="text"
-                  placeholder="Travelers "
-                  class="home-textinput3 input"
-                />
-              </div>
+
+            <div class="home-container014"></div>
+            <div class="home-container015"></div>
+            <div class="home-container016"></div>
+          </div>
+          <div class="home-select">
+            <input type="radio" name="radio" class="home-radiobutton1" />
+            <span class="home-text005">Multi-destination</span>
+          </div>
+          <select class="home-select1">
+            <option value="Option 1">Economy</option>
+            <option value="Option 1">Business</option>
+            <option value="Option 2">First</option>
+          </select>
+
+          <div class="home-container017">
+            <div class="home-container018">
+              <select class="home-textinput input" v-model="selectedSource">
+                <option disabled value="">Search by city or airport</option>
+                <option value="dubai">
+                  All airports in dubai: United Arab Emirates
+                </option>
+                <option value="istanbul">
+                  All airports in Istanbul: Turkey
+                </option>
+                <option value="tehran">All airports in Iran: Tehran</option>
+              </select>
             </div>
-            <h3>Source is : {{ selectedSource }}</h3>
-            <h3>Destination is : {{ selectedDestination }}</h3>
-            <h3>Date is : {{ selectedDate }}</h3>
-  
-            <div class="home-container027">
-              <!-- <NuxtLink to="fly-search" rel="noreferrer noopener" class="home-button02 button">Search →</NuxtLink> -->
-              <router-link
-                :to="{
-                  name: 'fly-search',
-                  query: {
-                    source: selectedSource,
-                    destination: selectedDestination,
-                    date: selectedDate,
-                  },
-                }"
-                class="home-button02 button"
+            <div class="home-container022">
+              <select
+                class="home-textinput1 input"
+                v-model="selectedDestination"
               >
-              Destination Now →
-              </router-link>
+                <option disabled value="">Search by city or airport</option>
+                <option value="dubai">
+                  All airports in dubai: United Arab Emirates
+                </option>
+                <option value="istanbul">
+                  All airports in Istanbul: Turkey
+                </option>
+                <option value="tehran">All airports in Iran: Tehran</option>
+              </select>
+            </div>
+            <div class="home-container024">
+              <input
+                type="date"
+                class="home-textinput2 input"
+                v-model="selectedDate"
+              />
+            </div>
+            <div class="home-container025">
+              <input
+                type="text"
+                placeholder="Travelers "
+                class="home-textinput3 input"
+              />
             </div>
           </div>
+          <!-- <h3>Source is : {{ selectedSource }}</h3>
+            <h3>Destination is : {{ selectedDestination }}</h3>
+            <h3>Date is : {{ selectedDate }}</h3> -->
+
+          <div class="home-container027">
+            <!-- <NuxtLink to="fly-search" rel="noreferrer noopener" class="home-button02 button">Search →</NuxtLink> -->
+            <router-link
+              :to="{
+                name: 'fly-search',
+                query: {
+                  source: selectedSource,
+                  destination: selectedDestination,
+                  date: selectedDate,
+                },
+              }"
+              class="home-button02 button"
+            >
+              Destination Now →
+            </router-link>
+          </div>
         </div>
-      </section>
+      </div>
+    </section>
+    <!---------->
+    <div class="home-container028">
+      <div class="home-container029">
+        <h1 class="home-text006"></h1>
+        <span class="home-text007"></span>
+        <span class="home-text008"> </span>
+      </div>
     </div>
-  </template>
+    <!---------->
+    <HomeRecomended />
+  </div>
+</template>
   
   <script>
-  import { ref } from "vue";
-  
-  export default {
-    setup() {
-      const selectedSource = ref("");
-      const selectedDestination = ref("");
-      const selectedDate = ref("");
-  
-      return {
-        selectedSource,
-        selectedDestination,
-        selectedDate,
-      };
-    },
-  };
-  </script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const selectedSource = ref("");
+    const selectedDestination = ref("");
+    const selectedDate = ref("");
+
+    return {
+      selectedSource,
+      selectedDestination,
+      selectedDate,
+    };
+  },
+};
+</script>
   
