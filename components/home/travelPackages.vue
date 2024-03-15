fd22a6b5e8a3e85cbf8a88c8ba2b0ee83ad3c59b
Elham Alizadeh <92359726+elhamalizadeh@users.noreply.github.com>
2024-03-16 00:12:03 +0330

    RF: edit home style

--- components/home/travelPackages.vue
+++ components/home/travelPackages.vue
@@ -1,131 +1,118 @@
 <template>
-    <div>
-        <div class="home-container117">
-          <div class="home-container118">
-            <span class="home-text070">Travel Packages</span>
-            <span class="home-text071">
-              Tourist packages are available to you
-            </span>
+  <div class="home-container117">
+    <div class="home-container118">
+      <span class="home-text070">Travel Packages</span>
+      <span class="home-text071"> Tourist packages are available to you </span>
+    </div>
+  </div>
+
+  <div class="home-container119">
+    <div class="home-container120">
+      <div></div>
+      <NuxtLink to="#" class="d">
+        <div class="home-container124">
+          <div class="home-container125" id="hide3"></div>
+          <div class="home-container125" id="hide1">
+            <span class="home-text079">IRAN Packages</span>
+            <span class="home-text080">Book Now</span>
           </div>
-        </div>
 
-
-        <div class="home-container119">
-          <div class="home-container120">
-		<div >
-	</div>	
-			   <NuxtLink to="#" class="d">
-            <div class="home-container124">
-				
-				<div class="home-container125" id="hide3">
-                
-              </div>
-              <div class="home-container125" id="hide1">
-                <span class="home-text079">IRAN Packages</span>
-                <span class="home-text080">Book Now</span>
-              </div>
-				
-				
-				<div class="hover01 column" >
-				  <figure>
+          <div class="hover01 column">
+            <figure>
               <img
                 alt="image"
                 src="../../public/images/external/baghat-03-1400w.jpg"
                 class="home-image32"
-              /></figure>
-            </div>
-              <div class="home-container126" id="hide2">
-                <span class="home-text081">642$</span>
-                <span class="home-text082">
-					<span>
-					<img
-                alt="image"
-                src="../../public/images/external/icone.png" class="icon"
-               
               />
-					</span>
-                  <span>with in 3 to 5 Working days</span>
-                  <br />
-                  <span>Valid till 60 days for issuing</span>
-                </span>
-              </div>
-            </div>
-				</NuxtLink>   
-				   
-				 <!-- <NuxtLink to="#" class="d"> -->
-            <div class="home-container124">
-				
-				<div class="home-container125" id="hide3">
-                
-              </div>
-              <div class="home-container125" id="hide1">
-                <span class="home-text079">MALAYSIA Packages</span>
-                <span class="home-text080">Book Now</span>
-              </div>
-				
-				
-				<div class="hover01 column" >
-				  <figure>
-             <img
-                alt="image"
-                src="../../public/images/external/baghat-02-1400w.jpg"
-                class="home-image30"
-              />
-					  </figure></div>
-              <div class="home-container126" id="hide2">
-                <span class="home-text081">642$</span>
-                <span class="home-text082">
-					<span>
-					<img
-                alt="image"
-                src="../../public/images/external/icone.png" class="icon"
-               
-              />
-					</span>
-            
-                    
-                    <span>with in 3 to 5 Working days</span>
-                  <br />
-                  <span>Valid till 60 days for issuing</span>
-                </span>
-              </div>
-            </div></div>
-				<!-- </NuxtLink>    -->
-			  
-			  <NuxtLink to="#" class="d">
-            <div class="home-container124">
-				
-				<div class="home-container125" id="hide3">
-                
-              </div>
-              <div class="home-container125" id="hide1">
-                <span class="home-text079">EGYPT Packages</span>
-                <span class="home-text080">Book Now</span>
-              </div>
-				<div class="hover01 column" >
-				  <figure>
+            </figure>
+          </div>
+          <div class="home-container126" id="hide2">
+            <span class="home-text081">642$</span>
+            <span class="home-text082">
+              <span>
+                <img
+                  alt="image"
+                  src="../../public/images/external/icone.png"
+                  class="icon"
+                />
+              </span>
+              <span>with in 3 to 5 Working days</span>
+              <br />
+              <span>Valid till 60 days for issuing</span>
+            </span>
+          </div>
+        </div>
+      </NuxtLink>
+
+      <!-- <NuxtLink to="#" class="d"> -->
+      <div class="home-container124">
+        <div class="home-container125" id="hide3"></div>
+        <div class="home-container125" id="hide1">
+          <span class="home-text079">MALAYSIA Packages</span>
+          <span class="home-text080">Book Now</span>
+        </div>
+
+        <div class="hover01 column">
+          <figure>
+            <img
+              alt="image"
+              src="../../public/images/external/baghat-02-1400w.jpg"
+              class="home-image30"
+            />
+          </figure>
+        </div>
+        <div class="home-container126" id="hide2">
+          <span class="home-text081">642$</span>
+          <span class="home-text082">
+            <span>
               <img
                 alt="image"
-                src="../../public/images/external/baghat-01-1400w.jpg"
-                class="home-image31"
-              /></figure></div>
-              <div class="home-container126" id="hide2">
-                <span class="home-text081">642$</span>
-                <span class="home-text082">
-					<span>
-					<img
-                alt="image"
-                src="../../public/images/external/icone.png" class="icon"
-               
+                src="../../public/images/external/icone.png"
+                class="icon"
               />
-					</span>
-                  <span>with in 3 to 5 Working days</span>
-                  <br />
-                  <span>Valid till 60 days for issuing</span>
-                </span>
-              </div>
-            </div>
-				</NuxtLink>   
-          </div>
+            </span>
+
+            <span>with in 3 to 5 Working days</span>
+            <br />
+            <span>Valid till 60 days for issuing</span>
+          </span>
+        </div>
+      </div>
     </div>
+    <!-- </NuxtLink>    -->
+
+    <NuxtLink to="#" class="d">
+      <div class="home-container124">
+        <div class="home-container125" id="hide3"></div>
+        <div class="home-container125" id="hide1">
+          <span class="home-text079">EGYPT Packages</span>
+          <span class="home-text080">Book Now</span>
+        </div>
+        <div class="hover01 column">
+          <figure>
+            <img
+              alt="image"
+              src="../../public/images/external/baghat-01-1400w.jpg"
+              class="home-image31"
+            />
+          </figure>
+        </div>
+        <div class="home-container126" id="hide2">
+          <span class="home-text081">642$</span>
+          <span class="home-text082">
+            <span>
+              <img
+                alt="image"
+                src="../../public/images/external/icone.png"
+                class="icon"
+              />
+            </span>
+            <span>with in 3 to 5 Working days</span>
+            <br />
+            <span>Valid till 60 days for issuing</span>
+          </span>
+        </div>
+      </div>
+    </NuxtLink>
+  </div>
 </template>
