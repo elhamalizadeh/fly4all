bd1ad0ef979538725128632189ef91530b576033
Elham Alizadeh <92359726+elhamalizadeh@users.noreply.github.com>
2024-04-12 14:55:40 +0330

    RF:install tailwindCss

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,14 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-
   css:[
     '~/assets/css/style.css',
    '~/assets/css/index.css',
   ],
 
   modules:[
-    '@pinia/nuxt'
+    '@pinia/nuxt',
+    '@nuxtjs/tailwindcss'
   ]
 
 
